cdc46e24d363a8777750de3aa2b71ed8b1dd3c59
Stephanymacedo <stephanymaceo@outlook.pt>
2021-06-23 11:04:43 -0300

    testando

---
+++ js/main.js
@@ -0,0 +1,101 @@
+//botao
+function botao(){
+    document.getElementById("agradecimento").innerHTML="Obrigado por clicar";
+    //console.log(document.getElementById("agradecimento"));
+  // alert("obrigado");
+}
+function redirecionar(){
+ window.open("https://github.com/stephanymacedo/");
+ //window.location.href="https://github.com/stephanymacedo/";
+}
+function trocar(){
+    alert ("trocar texto");
+}
+
+//TAG B DEIXA EM NEGRITO
+
+//funcoes
+ /*function soma (n1,n2){
+    return  n1+n2;
+}
+var validar=0;
+function validaIdade(idade){
+  
+    if(idade >=18){
+         validar=true;
+    }else {
+        validar= false;
+    }
+    return validar;
+}
+
+var idade= prompt("QUAL SUA IDADE?");
+validaIdade(idade)
+console.log(validar); */
+
+
+
+//data
+
+/* var d =new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes()); */
+
+//for
+
+/* var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}; */
+
+//laços de repetição
+
+/* var count =0;
+while(count<=5){
+    console.log(count);
+    alert (count);
+    count++;
+} */
+
+
+//condicionais  
+
+/* var idade= prompt("qual sua idade");//comando pronpt recebe uma pergunta 
+//var idade =18;
+if (idade>=18){
+    alert("maior de idade");
+}  else{
+alert("menor de idade");
+} */
+
+//arrey e dicionario
+
+/* var frutas =[{nome:"maça", cor:"vermelha"},{nome:"banana", cor:"amarela"}, {nome:"uva", cor:"roxa"}]
+console.log(fruta.nome);
+alert(frutas[1].nome); */
+
+
+/* var fruta ={nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor); */
+
+//var lista =["maça","pera", "laranja"]
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.toString());
+//console.log(lista.join("-"));
+
+
+//introdução
+
+//var nome= "Stephny Macedo";
+//var n1= 5;
+//var n2= 3;
+//var frase="japão é o melhor time do mundo";
+//alert (nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+ //console.log(n1*n2);
+ //console.log(frase.toLowerCase());
+ //alert(frase.replace("japão","Brasil"));
